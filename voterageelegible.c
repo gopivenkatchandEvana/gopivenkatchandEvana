4fa2e481d3795e172d2e8339cb03f5640fb4b950
gopivenkatchandEvana <gopichanduevana@gmail.com>
2024-10-21 12:18:14 +0530

    voterageelegible.c

---
+++ voterageelegible.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() {
+    int a;
+    int b=18;
+    printf("enter the age ");
+    scanf("%d",&a);
+    if (a>b)
+    {
+        printf("his or her vote is elegible");
+    }
+    else
+    printf("his or her vote is not elegible");
+}
