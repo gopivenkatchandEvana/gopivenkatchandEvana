764a534638b34010d6ddc4022834bb5f6171fb55
gopivenkatchandEvana <gopichanduevana@gmail.com>
2024-12-09 12:29:16 +0530

    Create diffofoddandeven.c

---
+++ diffofoddandeven.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    int i,n,sum1=0,sum2=0,diff;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {
+        if(i%2==0)
+        {
+            sum1=sum1+i;
+        }
+        else
+        {
+            sum2=sum2+i;
+        }
+        }
+        printf("the difference is %d",abs(sum1-sum2));
+}
