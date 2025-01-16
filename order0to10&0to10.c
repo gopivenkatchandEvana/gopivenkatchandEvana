b323586d0a19daeae3a2bef289b097f2855e97fc
gopivenkatchandEvana <gopichanduevana@gmail.com>
2025-01-16 20:23:35 +0530

    Create order0to10&0to10.c

---
+++ order0to10&0to10.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i;
+    for( i=0;i<=10;++i){
+        printf("%d ",i);}
+        printf("\n");
+        for(int j=10;j>=0;--j){
+            printf("%d ",j);
+        }
+    return 0;
+}
